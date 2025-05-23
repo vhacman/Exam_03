b1a0bccd386433c3013f842d0e12e91ff2ed8674
vhacman <hacmanvioricagabriela@gmail.com>
2025-05-23 16:58:25 +0200

    get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -168,33 +168,33 @@ char *get_next_line(int fd)
 }
 
 
-#include <stdio.h>
-#include <fcntl.h>
+// #include <stdio.h>
+// #include <fcntl.h>
+// //x testare
+// int main(int argc, char **argv)
+// {
+// 	int     fd;
+// 	char    *line;
 
-int main(int argc, char **argv)
-{
-	int     fd;
-	char    *line;
+// 	if (argc != 2)
+// 	{
+// 		printf("Usage: %s <file_path>\n", argv[0]);
+// 		return (1);
+// 	}
+// 	fd = open(argv[1], O_RDONLY);
+// 	if (fd < 0)
+// 	{
+// 		perror("Error opening file");
+// 		return (1);
+// 	}
+// 	while ((line = get_next_line(fd)))
+// 	{
+// 		printf("%s", line);
+// 		free(line);
+// 	}
+// 	close(fd);
+// 	return (0);
+// }
 
-	if (argc != 2)
-	{
-		printf("Usage: %s <file_path>\n", argv[0]);
-		return (1);
-	}
-	fd = open(argv[1], O_RDONLY);
-	if (fd < 0)
-	{
-		perror("Error opening file");
-		return (1);
-	}
-	while ((line = get_next_line(fd)))
-	{
-		printf("%s", line);
-		free(line);
-	}
-	close(fd);
-	return (0);
-}
-
-// x compilare --> gcc -Wall -Wextra -Werror -D BUFFER_SIZE=32 get_next_line.c main.c
+// // x compilare --> gcc -Wall -Wextra -Werror -D BUFFER_SIZE=32 get_next_line.c main.c
 
