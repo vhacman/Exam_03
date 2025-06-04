31c9304de7700247476c20088bd19eb7ba06ee74
vhacman <hacmanvioricagabriela@gmail.com>
2025-06-04 20:04:55 +0200

    ok

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,99 +1,91 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: vhacman <vhacman@student.42roma.it>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/23 16:31:18 by vhacman           #+#    #+#             */
-/*   Updated: 2025/06/04 12:47:18 by vhacman          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
 
-int	ft_putchar(char c)
+int ft_putchar(char c)
 {
-	write (1, &c, 1);
-	return (1);
+	write(1, &c, 1);
+	return 1;
 }
 
-int	ft_putstr(char	*str)
+int ft_putstr(char *str)
 {
-	int		count = 0;
-
-	if (!str)
-		return (ft_putstr("(null)"));
-	while (*str)
+	int count = 0;
+	if(str == NULL)
+		return(ft_putstr("(null)"));
+	while(*str)
 		count += ft_putchar(*str++);
-	return (count);
+	return count;
 }
 
-int	ft_putnbr(int nb)
+int ft_putnbr(int nb)
 {
-	long	num = nb;
-	int		count = 0;
+	long num = nb;
+	int count = 0;
 
-	if (num < 0)
+	if(num < 0)
 	{
-		count = count + ft_putchar('-');
+		count += ft_putchar('-');
 		num = -num;
 	}
-	if (num >= 10)
-		count = count + ft_putnbr(num / 10);
-	count = count + ft_putchar((num % 10) + '0');
-	return (count);
+	if(num >= 10)
+		count += ft_putnbr(num / 10);
+	count += ft_putchar((num % 10) + '0');
+	return count;
 }
 
-int	ft_puthex(unsigned int nb)
+
+int ft_puthex(unsigned int nb)
 {
-	int		count = 0;
-	char	*base = "0123456789abcdef";
-	if (nb >= 16)
-		ft_puthex(nb / 16);
-	count = count + ft_putchar(base[nb % 16]);
-	return (count);
+	int count = 0;
+	char *base = "0123456789abcdef";
+
+	if(nb >= 16)
+		count += ft_puthex(nb / 16);
+	count += ft_putchar(base[nb % 16]);
+	return count;
 }
 
-int	ft_printf(const char *format, ...)
+int ft_printf(const char *format, ...)
 {
-	va_list		args;
-	int			count = 0;
-	int			i = 0;
+	va_list args;
+	int     count = 0;
+	int     i = 0;
 
 	va_start(args, format);
-	while (format[i])
+	while(format[i])
 	{
-		if (format[i] == '%' && format[i + 1])
+		if(format[i] == '%' && format[i + 1])
 		{
 			i++;
-			if (format[i] == 's')
-				count = count + ft_putstr(va_arg(args, char *));
-			else if (format[i] == 'd')
-				count = count + ft_putnbr(va_arg(args, int));
-			else if (format[i] == 'x')
-				count = count + ft_puthex(va_arg(args, unsigned int));
+			if(format[i] == '%')
+				count += ft_putchar('%');
+			if(format[i] == 's')
+				count += ft_putstr(va_arg(args, char *));
+			else if(format[i] == 'd')
+				count += ft_putnbr(va_arg(args, int));
+			else if(format[i] == 'x')
+				count += ft_puthex(va_arg(args, unsigned int));
 			else
-				count = count + ft_putchar(format[i]);
+				count += ft_putchar(format[i]);
 		}
 		else
-			ft_putchar(format[i]);
+			count += ft_putchar(format[i]);
 		i++;
 	}
 	va_end(args);
 	return (count);
 }
 
-// // x testare
-// #include <stdio.h>
+// // // x testare
 // int	main(void)
 // {
 // 	ft_printf ("Test 01: Hello %s!\n", "world");
 // 	printf ("Test 01 printf: Hello %s!\n", "world");
 
 // 	ft_printf ("Test 02: %s\n", (char *)0);
-// 	printf ("Test 02 printf: %s\n", (char *)0);
+// 	char *nullstr = NULL;
+// 	printf("Test 02 printf: %s\n", nullstr ? nullstr : "(null)");
 
 // 	ft_printf ("Test 03: Number: %d\n", 123456);
 // 	printf ("Test 03 printf: Number: %d\n", 123456);
