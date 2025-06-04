8342788fff2d27613d26f7e87ad28632f1102850
vhacman <hacmanvioricagabriela@gmail.com>
2025-06-04 16:20:15 +0200

    ok

---
+++ testget_next_line/get_next_line.h
@@ -0,0 +1,19 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(const char *src);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *str);
+
+#endif
--- testprintf/ft_printf.c
+++ testprintf/ft_printf.c
@@ -1,33 +1,77 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+int ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return 1;
+}
+
+int ft_putstr(char *str)
+{
+	int count = 0;
+	if(str == NULL)
+		return(ft_putstr("(null)"));
+	while(*str)
+		count += ft_putchar(*str++);
+	return count;
+}
+
+int ft_putnbr(int nb)
+{
+	long num = nb;
+	int count = 0;
+
+	if(num < 0)
+	{
+		count += ft_putchar('-');
+		num = -num;
+	}
+	if(num >= 10)
+		count += ft_putnbr(nb / 10);
+	count += ft_putchar((nb % 10) + '0');
+	return count;
+}
+
+
+int ft_puthex(unsigned int nb)
+{
+	int count = 0;
+	char *base = "0123456789abcdef";
+
+	if(nb >= 10)
+		ft_puthex(nb / 16);
+	count = count + ft_putchar(base[nb % 16]);
+	return count;
+}
+
 int ft_printf(const char *format, ...)
 {
-    va_list args;
-    int     count = 0;
-    int     i = 0;
+	va_list args;
+	int     count = 0;
+	int     i = 0;
 
-    va_start(args, format);
-    while(format[i])
-    {
-        if(format[i] == '%' && format[i + 1])
-        {
-            i++;
-            if(format[i] == 's')
-                count = count + ft_putstr(va_arg(args, char *));
-            else if(format[i] == 'd')
-                count = count + ft_putnbr(va_arg(args, int));
-            else if(format[i] == 'x')
-                count = count + ft_puthex(va_arg(args, unsigned int));
-            else
-                count = count + ft_putchar(format[i]);
-        }
-        else
-            ft_putchar(format[i]);
-        i++;
-    }
-    va_end(args);
-    return (count);
+	va_start(args, format);
+	while(format[i])
+	{
+		if(format[i] == '%' && format[i + 1])
+		{
+			i++;
+			if(format[i] == 's')
+				count = count + ft_putstr(va_arg(args, char *));
+			else if(format[i] == 'd')
+				count = count + ft_putnbr(va_arg(args, int));
+			else if(format[i] == 'x')
+				count = count + ft_puthex(va_arg(args, unsigned int));
+			else
+				count = count + ft_putchar(format[i]);
+		}
+		else
+			ft_putchar(format[i]);
+		i++;
+	}
+	va_end(args);
+	return (count);
 }
 
 // // x testare
